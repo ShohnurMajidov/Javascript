eebcd8767ec48c7b051409f4b389f593b6ef1e7e
Shohnur <Joblack013@gmail.com>
2023-03-01 04:39:00 +0500

    Switch-case||for loop

--- lesson-3.js/index.js
+++ lesson-3.js/index.js
@@ -39,3 +39,73 @@ switch(a){
 //=========================================================================================
 
 
+// // For loop
+// Prime number
+let i = 27;
+    if(i%2==0 && i%i==0){
+
+        console.log(`${i} it's NOT   prime number`);
+
+    }else{
+        console.log(`${i} it's  prime number`);
+    }
+
+
+
+
+
+
+
+// program to check if a number is prime or not
+
+// take input from the user
+const number = parseInt(prompt("Enter a positive number: "));
+let isPrime = true;
+
+// check if number is equal to 1
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+
+// check if number is greater than 1
+else if (number > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+
+// check if number is less than 1
+else {
+    console.log("The number is not a prime number.");
+}
+
+
+
+
+// Multiplication table
+
+
+
+for(i=2;i<=10;i++){
+    console.log(`==================
+${i}lik karra jadvali
+==================`);
+   for(j=1; j<=10;j++){
+     console.log(`${i}*${j}=${i*j}`);
+   }
+}
+
+
+
+
