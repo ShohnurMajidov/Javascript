b75a97065477b11bed14fcd56c2ac30d2020a8a4
Shohnur <Joblack013@gmail.com>
2023-03-10 01:51:59 +0500

    While || Function

--- lesson-5.js/index.js
+++ lesson-5.js/index.js
@@ -47,7 +47,7 @@ let objj ={
 
 for(let i in objj){
   if(typeof objj[i] == 'boolean')
-  console.log(i);
+   console.log(`${i}:${objj[i]}`);
 }
 
 
@@ -64,7 +64,7 @@ let objj1 ={
 
 for(let i in objj1){
   if(typeof objj1[i] == 'string')
-  console.log(i);
+  console.log(`${i}:${objj1[i]}`);
 }
 
 
@@ -123,5 +123,59 @@ let salaries = {
   multiply(menu)
 
   console.log(menu);
+
+
+  //==============================================
+ 
+ 
+ 
+  function makeUser() {
+    return {
+      name : "John",
+      ref :this
+    };
+  }
+
+  let user = makeUser()
+
+  console.log(user.ref.name)
+
+
+
+  //==============================================
+  //Create a constructor function Calculator that creates objects with 3 methods:
+
+//read() prompts for two values and saves them as object properties with names a and b respectively.
+//sum() returns the sum of these properties.
+//mul() returns the multiplication product of these properties.
+
+function Calculator() {
+
+  this.read = function() {
+    this.a = 4;
+    this.b = 6;
+  };
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
+}
+//  Calculator.read();
+
+
+let calculator = new Calculator();
+calculator.read();
+
+console.log( "Sum=" + calculator.sum() )
+console.log( "Mul=" + calculator.mul() );
+
+//=============================================
+
+
+
 
 
